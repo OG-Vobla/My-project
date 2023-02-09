45ac63fb34a67db8e8de2cf4471ea7e121b31b17
Og_Vobla <gggg26705@gmail.com>
2023-02-09 17:12:48 +0300

    сделаны все окна

--- Assets/SettingsControl.cs
+++ Assets/SettingsControl.cs
@@ -1,18 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class SettingsControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+	// Start is called before the first frame update
+	[SerializeField] private GameObject panel;
+	bool IsOpen = false;
+	public void Open()
     {
-        
-    }
+		if (IsOpen)
+		{
+			IsOpen = false;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+			panel.GetComponent<Animator>().SetBool("IsOpen", IsOpen);
+			panel.GetComponent<RectTransform>().anchoredPosition = new Vector2(443.5f, -41f);
+		}
+		else
+		{
+			IsOpen = true;
+			panel.SetActive(true);
+			panel.GetComponent<Animator>().SetBool("IsOpen", IsOpen);
+			panel.GetComponent<RectTransform>().anchoredPosition = new Vector2(443.5f, 51.8f);
+		}
+		
+
+	}
 }
